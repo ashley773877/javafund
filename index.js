cca522e25623a0315516a0e262c11a015e546135
ashley773877 <mirandaashley16@gmail.com>
2023-11-30 13:46:56 -0500

    sample data

---
+++ index.js
@@ -0,0 +1,19 @@
+// sample
+const courseInfo = {
+    "id" : 1,
+    "name" : "math"
+};
+
+const assignmentGroup = {
+    "id": 1,
+    "name": "homework",
+    "course-id": 1,
+    "group-weight": 0.5,
+    "assignments": [
+        {
+            "id": 1,
+            "due-at": "2023-12-01/23",
+            "points-possible": 100,
+        }
+    ]
+}
