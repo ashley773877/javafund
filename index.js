81b41bc5b2f5567cf0e4ac8083beeb7401a0f634
ashley773877 <mirandaashley16@gmail.com>
2023-12-01 14:15:30 -0500

    adding assignments

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const courseInfo = {
     "name": "Computer Science 101",
   };
 
-  
+  const assignmentGroup = {
+    "id": 1,
+    "name": "Homework",
+    "course_id": 1,
+    "group_weight": 30,
+    "assignments": [
+      {
+        "id": 101,
+        "name": "Assignment 1",
+        "due_at": "2023-12-15T23:59:59Z",
+        "points_possible": 100,
+      },
+   
+    ],
+  };
 
