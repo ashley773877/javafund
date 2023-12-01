253b2ff01f525d02b049713a6986ab554cbe8f57
ashley773877 <mirandaashley16@gmail.com>
2023-12-01 14:14:37 -0500

    showing sample

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 // sample
 const courseInfo = {
-    "id" : 1,
-    "name" : "math"
-};
-
-const assignmentGroup = {
     "id": 1,
-    "name": "homework",
-    "course-id": 1,
-    "group-weight": 0.5,
-    "assignments": [
-        {
-            "id": 1,
-            "due-at": "2023-12-01/23",
-            "points-possible": 100,
-        }
-    ]
-}
+    "name": "Computer Science 101",
+  };
+
+  
+
